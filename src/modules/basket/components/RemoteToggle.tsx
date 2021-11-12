4a08f5f49814ccd7d005e510ff8a539150631fd9
PeterJFB <42469466+PeterJFB@users.noreply.github.com>
2021-11-12 10:51:35 +0100

    Create controller for basket-stream (#1)

---
+++ src/modules/basket/components/RemoteToggle.tsx
@@ -0,0 +1,68 @@
+import React, { FormEventHandler, ReactNode } from "react";
+import styles from "./RemoteToggle.module.css";
+import moduleinfo from "../moduleinfo.json";
+
+type CheckBoxButtonProps = {
+  children?: ReactNode;
+  id: string;
+  activatedEventName: string;
+  deactivatedEventName: string;
+};
+
+const RemoteCheckboxButton = ({
+  children,
+  id,
+  activatedEventName,
+  deactivatedEventName,
+}: CheckBoxButtonProps) => {
+  // Decide name/id of toggle based on which property is defined
+  id = id ? id : String.apply(children);
+  children = children ? children : id;
+
+  // Perform post with the new event. endpoint is defined in moduleinfo.json
+  const handleToggleEvent: FormEventHandler<HTMLInputElement> = (event) => {
+    const isActived = event.currentTarget.checked;
+
+    let responseData;
+
+    const requestOptions: RequestInit = {
+      method: "POST",
+      mode: "no-cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        event: isActived ? activatedEventName : deactivatedEventName,
+      }),
+    };
+
+    fetch(moduleinfo.url, requestOptions)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then(
+        (data) => {
+          responseData = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    console.log(responseData);
+
+    // if (!responseData) {
+    //   event.currentTarget.checked = !isActived;
+    //   // Display error ...
+    // }
+  };
+
+  return (
+    <div className={styles.CheckboxButton}>
+      <input type="checkbox" id={id} onChange={handleToggleEvent} />
+      <label htmlFor={id}>
+        <div>{children}</div>
+      </label>
+    </div>
+  );
+};
+
+export default RemoteCheckboxButton;
--- src/modules/basket/index.tsx
+++ src/modules/basket/index.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styles from "./Basket.module.css";
+import RemoteToggle from "./components/RemoteToggle";
+import moduleinfo from "./moduleinfo.json";
 
-const Basket: React.FC = () => {
-  return <div>Basket</div>;
+const Basket = () => {
+  console.log(moduleinfo);
+
+  const buttons = moduleinfo.eventGroups.map(
+    ({ name, activatedEventName, deactivatedEventName }) => (
+      <RemoteToggle
+        key={name}
+        id={name}
+        activatedEventName={activatedEventName}
+        deactivatedEventName={deactivatedEventName}
+      />
+    )
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.endpointText}>/basket</div>
+      <div className={styles.formWrapper}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className={styles.form}
+        >
+          {buttons}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Basket;
