e27f2d1e1ae668996f51493623e58e1ae686511b
Sindre Følling Sørensen <sindrefs@gmail.com>
2021-11-26 17:43:29 +0100

    Add redux and add URL override base functionality (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Basket from "./modules/basket";
+import Landing from "./modules/landing";
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/basket" element={<Basket />} />
-        </Routes>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/basket" element={<Basket />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
